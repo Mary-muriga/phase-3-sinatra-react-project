c0d74f8588c1942bda2aaaf0d1154d2cc7c838cd
Marymuriga <marymuriga@gmail.com>
2022-09-10 09:22:38 +0300

    added routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,63 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  #get "/pets/:id" do
-   # pet = Pet.find(params[:id])
-    #pet.to_json
-  #end
-
-  get "/pets/:id" do
-    pet = Pet.find(params[:id])
-    pet.to_json(include: :owners)
-  end
 
   get "/pets" do
-   pet = Pet.all
-   pet.to_json
+    pet = Pet.all
+    pet.to_json
+   end
+
+   get "/pets/:id" do
+     pet = Pet.find(params[:id])
+     pet.to_json
+   end
+
+  # get "/pets/:id" do
+  #    pet = Pet.find(params[:id])
+  #    pet.to_json(include: :owners)
+  #  end
+
+   delete "/pets/:id" do
+     pet = Pet.find(params[:id])
+     pet.destroy
+     pet.to_json
+   end
+
+  post '/pets' do
+    pet = Pet.create(
+      name: params[:name],
+      breed: params[:breed],
+      age: params[:age],
+      color: params[:color],
+      category: params[:category],
+      image_url: params[:image_url])
+    pet.to_json
+  end
+   
+  patch '/pets/:id' do
+    pet = Pet.find(params[:id])
+    pet.update(
+      color: params[:color],
+      category: params[:category]
+      )
+      pet.to_json
   end
 
- # get "/" do
-  #  { message: "Good luck with your project!" }.to_json
-  #end
+  get "/owners" do
+    owner = Owner.all
+    owner.to_json
+   end
 
+   get "/petlovers" do
+    petlover = Petlover.all
+    petlover.to_json
+   end
+
+   get "/petlovers/:id" do
+    petlover = Petlover.find(params[:id])
+    petlover.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-Pet.create(name: "Aura", breed: "Exotic shorthair", age: 1, color: "black", category: "female", image_url: "https://unsplash.com/s/photos/exotic-shorthair")
-Pet.create(name: "Pandora", breed: "Bengal", age: 1.5, color: "black & white", category: "female", image_url: "https://www.shutterstock.com/search/snow-bengal-cat")
-Pet.create(name: "Sophia", breed: "American curl", age: 2, color: "brown", category: "female", image_url: "https://www.desicomments.com/wallpapers/animal-wallpapers/light-brown-american-curl/")
-Pet.create(name: "Dalila", breed: "American shorthair", age: 3, color: "brown & white", category: "female", image_url: "http://www.vetstreet.com/cats/american-shorthair")
-Pet.create(name: "Hasina", breed: "Persian", age: 2.2, color: "white", category: "female", image_url: "https://www.alamy.com/beautiful-white-persian-cat-on-a-bed-image229857849.html")
-Pet.create(name: "Atlas", breed: "Exotic shorthair", age: 5, color: "white", category: "male", image_url: "https://www.istockphoto.com/photos/exotic-shorthair")
-Pet.create(name: "Andreas", breed: "Bengal", age: 3, color: "black", category: "male", image_url: "https://www.istockphoto.com/photos/black-bengal-cat")
-Pet.create(name: "Ares", breed: "American curl", age: 4, color: "grey", category: "male", image_url: "https://www.pinterest.com/chishuzkatmom/american-curl-cat/")
-Pet.create(name: "Balbina", breed: "American shorthair", age: 4, color: "grey", category: "male", image_url: "https://www.askideas.com/18-most-awesome-grey-american-shorthair-cat-photos-and-pictures/")
-Pet.create(name: "Gazelle", breed: "Persian", age: 1, color: "black", category: "male", image_url: "https://www.dreamstime.com/photos-images/black-persian-cat.html")
-Pet.create(name: "Ginger", breed: "sphynx", age: 2.5, color: "brown", category: "male", image_url: "https://unsplash.com/s/photos/sphinx-cat")
+Pet.create(name: "Aura", breed: "Exotic shorthair", age: 1, color: "black", category: "female", image_url: "https://i.pinimg.com/originals/8b/85/fa/8b85fa07b5b44ed2b65ff498d2d06d04.jpg")
+Pet.create(name: "Pandora", breed: "Bengal", age: 1.5, color: "black & white", category: "female", image_url: "https://image.shutterstock.com/image-photo/cute-white-little-bengal-kitten-260nw-1791732539.jpg")
+Pet.create(name: "Sophia", breed: "American curl", age: 2, color: "brown", category: "female", image_url: "https://www.thesprucepets.com/thmb/XbS_nf98Li1tfmBhbHd2gY_nogk=/1000x1000/smart/filters:no_upscale()/american-curl-full-profile-history-and-care-4705972-hero-2c9bdcfba3d84130b8eed233c46c19d3.jpg")
+Pet.create(name: "Dalila", breed: "American shorthair", age: 3, color: "brown & white", category: "female", image_url: "https://www.hepper.com/wp-content/uploads/2021/08/Exotic-Shorthair-Cat-sitting-by-the-window_Wutlufaipy_Shutterstock.jpg")
+Pet.create(name: "Hasina", breed: "Persian", age: 2.2, color: "white", category: "female", image_url: "https://st2.depositphotos.com/2289871/5465/i/950/depositphotos_54654119-stock-photo-white-persian-cat-walking.jpg")
+Pet.create(name: "Atlas", breed: "Exotic shorthair", age: 5, color: "white", category: "male", image_url: "https://image.shutterstock.com/image-photo/white-exotic-shorthair-cat-laying-260nw-1047983233.jpg")
+Pet.create(name: "Andreas", breed: "Bengal", age: 3, color: "black", category: "male", image_url: "https://i.pinimg.com/236x/d9/94/1a/d9941a321454571a9fe956ea994059a4--bengal-cat-breeders-toyger-cat.jpg")
+Pet.create(name: "Ares", breed: "American curl", age: 4, color: "grey", category: "male", image_url: "https://d83vwgwvylun0.cloudfront.net/img/s/402-american-curl.jpg")
+Pet.create(name: "Balbina", breed: "American shorthair", age: 4, color: "grey", category: "male", image_url: "https://thumbs.dreamstime.com/b/american-shorthair-cat-colored-backgrounds-studio-photography-129325157.jpg")
+Pet.create(name: "Gazelle", breed: "Persian", age: 1, color: "black", category: "male", image_url: "https://image.shutterstock.com/image-photo/persian-cat-9-months-old-260nw-72113560.jpg")
+Pet.create(name: "Ginger", breed: "sphynx", age: 2.5, color: "brown", category: "male", image_url: "https://i.pinimg.com/736x/cf/cf/93/cfcf9329ef691df56b248eaee152bdeb.jpg")
 
 Owner.create(name: "Mariam", contact: "098765555", address: "Nairobi", firm: "Cats Adoption Centre")
 
